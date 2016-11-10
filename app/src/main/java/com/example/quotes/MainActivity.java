3278fff8f6bdc2d97e852ec81a5c8086c274442f
NobleAries <michalasakura@gmail.com>
2016-11-10 17:08:46 +0100

    ISSUE-3: Changed MainActivity to extend AppCompatActivity. Added the app bar with search and settings

--- app/src/main/java/com/example/quotes/MainActivity.java
+++ app/src/main/java/com/example/quotes/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.quotes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        // TODO Add TextListener do searchView
+
+        return true;
     }
 }
