276367c060c8dc39375d20f0934759a82e5a5b98
Piotr Przybyło <pitu5599@gmail.com>
2016-11-13 13:36:06 +0100

    ISSUE-2: Added sample icons to tabs

--- app/src/androidTest/java/com/example/quotes/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/quotes/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.quotes", appContext.getPackageName());
+        assertEquals("com.example.quotes_icon", appContext.getPackageName());
     }
 }
