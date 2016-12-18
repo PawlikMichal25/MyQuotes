2355077aba315ca35bae0fa4f3d499f57566e227
Piotr Przybyło <pitu5599@gmail.com>
2016-12-18 22:36:54 +0100

    ISSUE-12: Quotes are now loaded from database

--- app/src/main/java/com/example/quotes/QuotesDatabaseHelper.java
+++ app/src/main/java/com/example/quotes/QuotesDatabaseHelper.java
@@ -1,9 +1,12 @@
 package com.example.quotes;
 
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.Date;
 
 public class QuotesDatabaseHelper extends SQLiteOpenHelper {
 
@@ -24,14 +27,41 @@ public class QuotesDatabaseHelper extends SQLiteOpenHelper {
                 "_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 "author_id INTEGER NOT NULL, " +
                 "content TEXT NOT NULL, " +
-                "favorite BOOLEAN NOT NULL" +
+                "favorite BOOLEAN NOT NULL," +
                 "date DATE, " +
-                "FOREIGN KEY(author_id) REFERENCES Authors(_id);");
-
+                "FOREIGN KEY(author_id) REFERENCES Authors(_id));");
+        setUpSampleData(db);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    private void setUpSampleData(SQLiteDatabase db) {
+        long id1 = insertAuthor(db, "Jose", "Mourinho");
+        long id2 = insertAuthor(db, "John", "Adams");
+        long id3 = insertAuthor(db, "Peter", "Richards");
+        insertQuote(db, id1, "I think I'm a special one", true);
+        insertQuote(db, id2, "Be happy", false);
+        insertQuote(db, id3, "Have a hobby", false);
+    }
+
+
+    private long insertAuthor(SQLiteDatabase db, String name, String surname){
+        ContentValues values = new ContentValues();
+        values.put("name", name);
+        values.put("surname", surname);
+        long id = db.insert("Authors", null, values);
+        return id;
+    }
+
+    private void insertQuote(SQLiteDatabase db, long authorID, String content, boolean isFavorite){
+        ContentValues values = new ContentValues();
+        values.put("author_id", authorID);
+        values.put("content", content);
+        values.put("favorite", isFavorite);
+        values.put("date", String.valueOf(new Date()));
+        db.insert("Quotes", null, values);
     }
 }
