9dfc487429ea55ee389ff716527b0272191d67ec
NobleAries <michalasakura@gmail.com>
2016-12-30 19:27:06 +0100

    ISSUE-18: Created new activity for concrete author

---
+++ app/src/main/java/com/example/quotes/AuthorsActivity.java
@@ -0,0 +1,26 @@
+package com.example.quotes;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuthorsActivity extends AppCompatActivity {
+
+    public static final String AUTHOR_ID = "authorId";
+    public static final String AUTHOR_NAME = "authorName";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authors);
+
+        getSupportActionBar().setTitle((String)getIntent().getExtras().get(AUTHOR_NAME));
+
+        QuotesFragment quotesFragment = new QuotesFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, quotesFragment);
+        transaction.addToBackStack(null);
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        transaction.commit();
+    }
+}
