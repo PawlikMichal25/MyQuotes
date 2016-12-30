97c88409eed16180ac1f00bf998caba6eae45b55
NobleAries <michalasakura@gmail.com>
2016-12-30 19:28:31 +0100

    ISSUE-18: Upgraded authors fragment with cursor adapter and listener

--- app/src/main/java/com/example/quotes/QuotesFragment.java
+++ app/src/main/java/com/example/quotes/QuotesFragment.java
@@ -28,6 +28,8 @@ public class QuotesFragment extends Fragment {
     private RecyclerView recyclerView;
     private QuotesAdapter quotesAdapter;
     private List<Quote> quotes;
+    private Cursor quotesCursor;
+    private SQLiteDatabase db;
 
     public QuotesFragment() {}
 
@@ -51,27 +53,25 @@ public class QuotesFragment extends Fragment {
     }
 
     private void initDataSet() {
-        quotes = new ArrayList<Quote>();
+        quotes = new ArrayList<>();
         try {
             SQLiteOpenHelper dbHelper = new QuotesDatabaseHelper(getActivity());
-            SQLiteDatabase db = dbHelper.getReadableDatabase();
-            Cursor cursor = db.rawQuery("SELECT Author_id, Content, Favorite FROM Quotes", null);
-            if (cursor.moveToFirst()){
+            db = dbHelper.getReadableDatabase();
+            quotesCursor = db.rawQuery("SELECT Author_id, Content, Favorite FROM Quotes", null);
+            if (quotesCursor.moveToFirst()){
                 do{
-                    int authorID = cursor.getInt(0);
+                    int authorID = quotesCursor.getInt(0);
                     Cursor authorCursor = db.query("Authors", new String[]{"FirstName", "LastName"},
                             "_id = ?", new String[]{String.valueOf(authorID)}, null, null, null);
                     if (authorCursor.moveToFirst()){
                         Author author = new Author(authorCursor.getString(0),
                                 authorCursor.getString(1));
-                        Quote quote = new Quote(author, cursor.getString(1), cursor.getInt(2) == 1);
+                        Quote quote = new Quote(author, quotesCursor.getString(1), quotesCursor.getInt(2) == 1);
                         quotes.add(quote);
                     }
                     authorCursor.close();
-                } while(cursor.moveToNext());
+                } while(quotesCursor.moveToNext());
             }
-            cursor.close();
-            db.close();
         } catch (SQLiteException e){
             Toast toast = Toast.makeText(getActivity(), "Database unavailable", Toast.LENGTH_LONG);
             toast.show();
@@ -94,4 +94,10 @@ public class QuotesFragment extends Fragment {
         recyclerView.setAdapter(quotesAdapter);
     }
 
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        quotesCursor.close();
+        db.close();
+    }
 }
