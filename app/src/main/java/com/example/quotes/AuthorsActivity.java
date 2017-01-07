1754102032201845d67099007f6f682c53a28718
NobleAries <michalasakura@gmail.com>
2017-01-07 15:47:44 +0100

    ISSUE-18: Changed QuotesFragment to be able to show just one author's quotes

--- app/src/main/java/com/example/quotes/AuthorsActivity.java
+++ app/src/main/java/com/example/quotes/AuthorsActivity.java
@@ -4,6 +4,8 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.quotes.model.Author;
+
 public class AuthorsActivity extends AppCompatActivity {
 
     public static final String AUTHOR_ID = "authorId";
@@ -17,6 +19,11 @@ public class AuthorsActivity extends AppCompatActivity {
         getSupportActionBar().setTitle((String)getIntent().getExtras().get(AUTHOR_NAME));
 
         QuotesFragment quotesFragment = new QuotesFragment();
+
+        String [] authorNames = ((String)getIntent().getExtras().get(AUTHOR_NAME)).split(" ", 2);
+        quotesFragment.setAuthorData(new Author(authorNames[1], authorNames[0]),
+                (long)getIntent().getExtras().get(AUTHOR_ID));
+
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.fragment_container, quotesFragment);
         transaction.commit();
