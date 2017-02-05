bc38f85043b607448c2080084c53cdead1ced0e4
NobleAries <michalasakura@gmail.com>
2017-02-05 23:06:40 +0100

    ISSUE-23: Fixed bug on older APIs with opening google play

--- app/src/main/java/com/example/quotes/AboutActivity.java
+++ app/src/main/java/com/example/quotes/AboutActivity.java
@@ -1,6 +1,7 @@
 package com.example.quotes;
 
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,8 +18,14 @@ public class AboutActivity extends AppCompatActivity{
 
     public void onRateUsClick(View view){
         Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("market://details?id=com.google.android.apps.maps"));
-        // TODO Change package name to proper one. As for now it is: id=com.example.quotes
-        startActivity(intent);
+        intent.setData(Uri.parse("market://details?id=com.google.android.apps.maps"));  // TODO this.getPackageName()
+
+        try{
+            startActivity(intent);
+        }
+        catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=" + "com.google.android.apps.maps")));  // TODO this.getPackageName()
+        }
     }
 }
