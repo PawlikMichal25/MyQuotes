094aef39f6e26b25c7ec871a60b16111633db6ab
NobleAries <michalasakura@gmail.com>
2017-02-07 14:37:03 +0100

    ISSUE-24: Fields are now filled with proper values when editing quote

--- app/src/main/java/com/example/quotes/QuotesActivity.java
+++ app/src/main/java/com/example/quotes/QuotesActivity.java
@@ -5,27 +5,49 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class QuotesActivity extends AppCompatActivity {
 
     public static final String AUTHOR_FIRST_NAME = "AUTHOR_FIRST_NAME";
     public static final String AUTHOR_LAST_NAME = "AUTHOR_LAST_NAME";
+    public static final String IS_FAVORITE = "IS_FAVORITE";
     public static final String QUOTE_CONTENT = "QUOTE_CONTENT";
 
     private String authorFirstName;
     private String authorLastName;
+    private boolean isFavorite;
     private String quoteContent;
+
+    private EditText authorFirstNameView;
+    private EditText authorLastNameView;
+    private CheckBox isFavoriteView;
+    private EditText quoteContentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quotes);
+
+        authorFirstNameView = (EditText) findViewById(R.id.quotes_author_first_name);
+        authorLastNameView = (EditText) findViewById(R.id.quotes_author_last_name);
+        isFavoriteView = (CheckBox) findViewById(R.id.quotes_is_favorite);
+        quoteContentView = (EditText) findViewById(R.id.quotes_quote);
+
         Intent intent = getIntent();
         if(intentHasAllExtras(intent)) {
             authorFirstName = getIntent().getStringExtra(AUTHOR_FIRST_NAME);
             authorLastName = getIntent().getStringExtra(AUTHOR_LAST_NAME);
+            isFavorite = getIntent().getBooleanExtra(IS_FAVORITE, false);
             quoteContent = getIntent().getStringExtra(QUOTE_CONTENT);
+
+            authorFirstNameView.setText(authorFirstName);
+            authorLastNameView.setText(authorLastName);
+            isFavoriteView.setChecked(isFavorite);
+            quoteContentView.setText(quoteContent);
+
             getSupportActionBar().setTitle(R.string.title_activity_quotes_edit);
         }
     }
@@ -54,4 +76,5 @@ public class QuotesActivity extends AppCompatActivity {
     private boolean intentHasAllExtras(Intent intent){
         return intent.hasExtra(AUTHOR_FIRST_NAME) && intent.hasExtra(AUTHOR_LAST_NAME) && intent.hasExtra(QUOTE_CONTENT);
     }
+
 }
