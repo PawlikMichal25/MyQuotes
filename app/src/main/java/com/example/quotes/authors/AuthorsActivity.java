f9906bd727732d1c858a7aac613d1d931ae232b9
NobleAries <michalasakura@gmail.com>
2017-02-08 23:05:28 +0100

    ISSUE-24: Fixed bug with refreshing specific author activity after editing their quote

--- app/src/main/java/com/example/quotes/authors/AuthorsActivity.java
+++ app/src/main/java/com/example/quotes/authors/AuthorsActivity.java
@@ -1,5 +1,6 @@
 package com.example.quotes.authors;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,8 @@ public class AuthorsActivity extends AppCompatActivity {
     public static final String AUTHOR_ID = "authorId";
     public static final String AUTHOR_NAME = "authorName";
 
+    private QuotesFragment quotesFragment;  // For onActivityResult, alternatively we could add a tag during committing
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,14 +23,21 @@ public class AuthorsActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle((String)getIntent().getExtras().get(AUTHOR_NAME));
 
-        QuotesFragment quotesFragment = new QuotesFragment();
+        quotesFragment = new QuotesFragment();
 
         String [] authorNames = ((String)getIntent().getExtras().get(AUTHOR_NAME)).split(" ", 2);
-        quotesFragment.setAuthorData(new Author(authorNames[1], authorNames[0]),
-                (long)getIntent().getExtras().get(AUTHOR_ID));
+
+        quotesFragment.setAuthor(new Author(authorNames[1], authorNames[0]));
+        quotesFragment.setAuthorId((long)getIntent().getExtras().get(AUTHOR_ID));
 
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.fragment_container, quotesFragment);
         transaction.commit();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        quotesFragment.onActivityResult(requestCode, resultCode, data);
+        getSupportActionBar().setTitle(quotesFragment.getAuthor().toString());
     }
 }
